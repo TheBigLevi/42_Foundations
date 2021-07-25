611bf5009bc2f812b7e81d93a4f033f3829bdfa1
Lachlan Read <lachlan-read@protonmail.com>
2021-07-25 22:22:37 +0930

    Completed strcat, strcpy, strdup, strlen, strncpy and updated libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -5,4 +5,11 @@
 # include <unistd.h>
 # include <string.h>
 
+size_t  ft_strlen(const char *s);
+char    *ft_strdup(const char *s);
+char    *ft_strcpy(char *dest, const char *src);
+char    *ft_strncpy(char  *dest, const char *src, size_t n);
+char    *ft_strcat(char *dest, const char *src);
+
+
 #endif
