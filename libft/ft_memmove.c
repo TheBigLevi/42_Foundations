eb9e288acc8da08289a9decc6acadbb6b8d17e41
Lachlan Read <lreadtest@F2K023.42adel.org.au>
2021-08-07 15:27:12 +0930

    Altered and updated functions for norm

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -2,4 +2,13 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
 }
