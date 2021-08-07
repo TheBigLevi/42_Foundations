f47a51f32a3425ca32d1d8e0150d65948c8ef860
Lachlan Read <lreadtest@F2K023.42adel.org.au>
2021-08-07 18:12:47 +0930

    Added atoi and updated libft.h with prototypes

--- libft/libft.h
+++ libft/libft.h
@@ -5,6 +5,8 @@
 # include <unistd.h>
 # include <string.h>
 
+# define ISSPACE(c) (c==' '||c=='\t'||c=='\n'||c=='\r'||c=='\f'||c=='\v')
+
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -28,5 +30,13 @@ char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
