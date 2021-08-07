a928bccd6481e24fca45f618ab4dffdd369fe758
Lachlan Read <lreadtest@F2K023.42adel.org.au>
2021-08-07 16:56:16 +0930

    Updated libft with previously pushed functions

--- libft/libft.h
+++ libft/libft.h
@@ -15,7 +15,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 //void	ft_memcmp(consy char *s1, const char *s2);
 
-size_t	ft_strlen(const char *s);
+size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
@@ -23,5 +23,10 @@ char	*ft_strcat(char *dest, const char *src);
 char	*ft_strncat(char *dest, const char *src, size_t n);
 size_t	*ft_strlcat(char *dst, const char *src, size_t n);
 char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
