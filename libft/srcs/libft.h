6a08d0807f03887b7b85cc578a0ee12ecce619ee
Lachlan Read <lreadtest@F2N059.42adel.org.au>
2021-08-08 11:42:52 +0930

    Moved files to correct directories

---
+++ libft/srcs/libft.h
@@ -0,0 +1,45 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+# define ISSPACE(c) (c==' '||c=='\t'||c=='\n'||c=='\r'||c=='\f'||c=='\v')
+
+void	*ft_memset(void *str, int c, size_t n);
+void	ft_bzero(void *str, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+// TODO
+//void	*ft_memccpy(void dst, const void src, int c, size_t n);
+//void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
+//void	ft_memcmp(consy char *s1, const char *s2);
+
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strcat(char *dest, const char *src);
+char	*ft_strncat(char *dest, const char *src, size_t n);
+size_t	*ft_strlcat(char *dst, const char *src, size_t n);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+//--------ADDITIONAL FUNCTIONS--------//
+void	*ft_memalloc(size_t size);
+
+#endif
