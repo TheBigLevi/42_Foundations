714660af3d758325914edde3f71c8c0c0e66fae3
Lachlan Read <lreadtest@F2N059.42adel.org.au>
2021-08-08 12:14:12 +0930

    Finished ft_calloc.c

--- libft/srcs/ft_calloc.c
+++ libft/srcs/ft_calloc.c
@@ -2,5 +2,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	void	*ptr;
 
+	ptr = (void *)malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, size * count);
+	return (ptr);
 }
