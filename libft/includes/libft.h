9663d25e7b01343623e7eb1fac534e5bbc128337
Lachlan Read <lreadtest@F2N057.42adel.org.au>
2021-08-10 16:56:29 +0930

    Restarted ft_split, and upadted libft with prototypes and norm

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -49,7 +49,14 @@ void	*ft_calloc(size_t count, size_t size);
 
 //--------ADDITIONAL FUNCTIONS--------//
 
+//CHECK len == 0!!!
 char	*ft_substr(char const *str, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	ft_split(char const *str, char c);
+
+char	*ft_strmapi(char const *str, char (*f)(unsigned int, char));
+
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *str, int fd);
 void	ft_putendl_fd(char *str, int fd);
@@ -64,6 +71,6 @@ void	ft_lstadd_front(t_list **alst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **alst, t_list *new);
-t_list	ft_lstdelone(t_list *lst, void (*del)(void *));
+//t_list	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
--- libft/srcs/ft_lstdelone.c
+++ libft/srcs/ft_lstdelone.c
@@ -1,9 +1,1 @@
 #include "libft.h"
-
-char	*ft_substr(char const *str, unsigned int start, size_t len)
-{
-	char	c;
-
-	c = 48;
-	return (c);
-}
