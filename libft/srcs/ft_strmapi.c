bb715dab74326c21eed5a3b8a77c860a17b6d83a
Lachlan Read <lreadtest@F2K022.42adel.org.au>
2021-08-11 16:48:13 +0930

    Completed ft_strmapi.c

--- libft/srcs/ft_strmapi.c
+++ libft/srcs/ft_strmapi.c
@@ -2,4 +2,18 @@
 
 char	*ft_strmapi(char const *str, char (*f)(unsigned int, char))
 {
+	unsigned int	i;
+	char			*dest;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
+	while (str[i] != '\0')
+	{
+		dest[i] = f(i, str[i]);
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
