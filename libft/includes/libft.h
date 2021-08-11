b2ddac2a50fd47b79fc9d7b03c3447a2ea6bb480
Lachlan Read <lreadtest@F2K022.42adel.org.au>
2021-08-11 13:59:23 +0930

    Moved bonus functions to seperate file as specified by provided material

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -53,7 +53,7 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *str, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	ft_split(char const *str, char c);
+char	**ft_split(char const *str, char c);
 
 char	*ft_strmapi(char const *str, char (*f)(unsigned int, char));
 
