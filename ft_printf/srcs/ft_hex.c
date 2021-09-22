93cffaf5706940987f509d1d09266879b65fc0d1
Lachlan Read <lread@F3C026.42adel.org.au>
2021-09-22 21:09:40 +0930

    Removed <stdio.h>

--- ft_printf/srcs/ft_hex.c
+++ ft_printf/srcs/ft_hex.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 static char	*ft_ptrstr(unsigned long n, int *k)
 {
